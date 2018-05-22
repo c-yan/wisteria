28c7644f4191ccb2b6335c84e2881ded559f8e4c
c-yan <c-yan@users.noreply.github.com>
2018-05-22 21:45:58 +0900

    不要な uses の削除

--- Process.pas
+++ Process.pas
@@ -3,8 +3,8 @@ unit Process;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls;
+  System.SysUtils, System.Classes, Vcl.Controls, Vcl.Forms, Vcl.StdCtrls,
+  Vcl.ComCtrls;
 
 type
   TProcessForm = class(TForm)
