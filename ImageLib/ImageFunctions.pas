44edbe0c66ea152c8ca543edefe2a7e5d6cf37cd
c-yan <c-yan@users.noreply.github.com>
2018-06-06 19:21:18 +0900

    Int64 から Single に暗黙的にキャストされて Clamp が呼ばれている箇所があったのを修正

--- ImageLib/ImageFunctions.pas
+++ ImageLib/ImageFunctions.pas
@@ -6,6 +6,7 @@ uses
   Winapi.Windows, Vcl.Graphics, ImageTypes;
 
 procedure NullProgressProc(Progress: Integer);
+function Clamp(Value, Min, Max: Int64): Int64; inline; overload;
 function Clamp(Value, Min, Max: Integer): Integer; inline; overload;
 function Clamp(Value, Min, Max: Single): Single; inline; overload;
 function Clamp(Value, Min, Max: Double): Double; inline; overload;
@@ -15,6 +16,13 @@ implementation
 
 procedure NullProgressProc(Progress: Integer);
 begin
+end;
+
+function Clamp(Value, Min, Max: Int64): Int64; inline; overload;
+begin
+  if Value < Min then Result := Min
+  else if Value > Max then Result := Max
+  else Result := Value;
 end;
 
 function Clamp(Value, Min, Max: Integer): Integer; inline; overload;
